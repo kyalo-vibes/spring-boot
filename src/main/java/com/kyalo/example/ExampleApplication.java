d28613e9ebaac990858fc38a3df967edfdc00fcf
kyalo-vibes <kevin.kioko4@gmail.com>
2024-10-29 15:47:05 +0300

    Set up for Spring Rest

--- src/main/java/com/kyalo/example/ExampleApplication.java
+++ src/main/java/com/kyalo/example/ExampleApplication.java
@@ -10,21 +10,6 @@ import java.util.Collections;
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-
-		var app = new SpringApplication(ExampleApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev, test"));
-		var ctx = app.run(args);
-
-//		MyFirstClass myFirstClass = ctx.getBean("myBean", MyFirstClass.class);
-//		System.out.println(myFirstClass.sayHello());
-
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellStory());
-		System.out.println(myFirstService.getCustomProperty());
-		System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
-		System.out.println(myFirstService.getCustomPropertyFromMultiple());
-//		System.out.println(myFirstService.getJavaVersion());
-//		System.out.println(myFirstService.getOSName());
-//		System.out.println(myFirstService.getCustomProperty());
+		SpringApplication.run(ExampleApplication.class);
 	}
 }
