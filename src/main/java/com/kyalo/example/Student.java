d53b1eb44731fd5a2d7ea5b99fae1f9c977c7048
kyalo-vibes <kevin.kioko4@gmail.com>
2024-10-30 08:26:35 +0300

    Implemented entity

---
+++ src/main/java/com/kyalo/example/Student.java
@@ -0,0 +1,74 @@
+package com.kyalo.example;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "T_STUDENT")
+public class Student {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(
+            name = "FIRST_NAME",
+            length = 25
+    )
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private int age;
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
